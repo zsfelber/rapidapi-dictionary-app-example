3bf206cdabbe1ac82c7826f5fb3dc99c68d85b3c
zsfelber@gmail.com <zsfelber@gmail.com>
2022-02-05 09:11:08 +0100

    ..

--- functions/getWord.js
+++ functions/getWord.js
@@ -1,285 +1,21 @@
 const axios = require('axios');
 const fs = require('fs');
+const crawler = require('crawler');
+
 const MAX_WORDS = 50;
 const CACHE_CLUSTERS = false;
 const MAX_NODE_FREQUENCY = 4;
 const TRAVERSE_SIMILAR = false;
 
-function singleWordToDisplay(data) {
-
-  // create new array to push data to
-  let results = [];
-  let result = {
-    frequency:data.frequency,
-    pronunciation:data.pronunciation,
-    results, etc:""
-  };
-
-  if (data.results) data.results.map(def => {
-    let definitionArray = [];
-    let definition = {
-      partOfSpeech:def.partOfSpeech,
-      properties:definitionArray
-    };
-
-    // creates array of keys in object
-    const keys = Object.keys(def);
-    keys.map(key => {
-
-      // builds regex that looks for capital letters
-      // The response parameters are in camelCase, so we need to ID
-      // the capital letters and add spaces instead so the 
-      // front end can easily display the parameter labels
-      const regex = /(?=[A-Z])/;
-
-      // creates presentable label
-      const label = key.split(regex).join(' ').toLowerCase();
-
-      // grabs the value for the parameter from the
-      // definition object in response
-      const value = def[key];
-
-      // constructs new object to send to frontend
-      let newObj = {
-        label,
-        value,
-        isString: typeof value === 'string' ? true : false
-      };
-
-      definitionArray.push(newObj);
-    })
-
-    results.push(definition);
-  });
-
-  return result;
-}
-
-async function loadSingleWord(word, asobject) {
-
-  const wfpath = `cache/words/${word}`;
-
-  if (fs.existsSync(wfpath)) {
-
-    //console.log("From cache file/single "+wfpath+"  asobject:"+asobject+"...\n");
-    let ijson = fs.readFileSync(wfpath).toString();
-    try {
-      let data = JSON.parse(ijson);
-
-      if (asobject) {
-        return data;
-      } else {
-        let result = singleWordToDisplay(data);
-        const ojson = JSON.stringify(result);         // modified
-        return ojson;
-      }
-  
-    } catch (e) {
-      console.warn("Delete invalid file : "+wfpath, e);
-      fs.unlinkSync(wfpath);
-    }
-  }
-
-  // send request to the WordsAPI
-  const response = await axios({
-    "method":"GET",
-    "url":`https://wordsapiv1.p.rapidapi.com/words/${word}`,
-    "headers":{
-    "content-type":"application/octet-stream",
-    "x-rapidapi-host":"wordsapiv1.p.rapidapi.com",
-    "x-rapidapi-key":process.env.RAPIDAPI_KEY
-    }
-  });
-
-  const djson = JSON.stringify(response.data);  // original
-  fs.writeFile(wfpath, djson, (err) => {
-    if (err) {
-      console.error("Cache file/single "+wfpath+"  asobject:"+asobject+" write failure : "+err+"\n");
-    } else {
-      console.log("Cache file/single "+wfpath+"  asobject:"+asobject+" written successfully\n");
-    }
-  });
-
-  if (asobject) {
-    return response.data;
-  } else {
-    let result = singleWordToDisplay(response.data);
-    const ojson = JSON.stringify(result);         // modified
-    return ojson;
-  }
-
-}
-
-class TraverseNode {
-
-  entry;val;level;partOfSpeech;
-  definition;synonyms;similar;words;
-  key;
-
-  constructor(by_def, entry, val, level) {
-    this.entry=entry;this.val=val;this.level=level;
-
-    this.definition = val.definition; 
-    this.synonyms = [];
-    this.similar = [];
-    this.words = [];
-
-    this.add(by_def);
-  }
-
-  add(by_def) {
-  
-    this.partOfSpeech = this.val.partOfSpeech;
-    this.synonyms.push.apply(this.synonyms, this.val.synonyms);
-    this.synonyms.push(this.entry.word);
-    this.synonyms.sort();
-  
-    this.similar.push.apply(this.similar, this.val.similarTo);
-    this.similar.sort();
-  
-    this.words.push.apply(this.words, this.synonyms);
-    this.words.push.apply(this.words, this.similar);
-  
-    this.key = this.level+":::::::"+this.synonyms.length+"::::::"+this.synonyms.join(", ");
-
-    if (!by_def[this.val.definition]) {
-
-      by_def[this.val.definition] = this;
-    }
-  }
-
-  compress() {
-    delete this.val;
-    delete this.key;
-    delete this.words;
-  }
-  
-};
-
-async function loadDictionaryAndChildren(tresult, word, traversion) {
-
-  const by_def = tresult.by_def;
-  const entry = await loadSingleWord(word, true);
-
-  if (traversion.level > 1 && 
-      entry.frequency && entry.frequency>=MAX_NODE_FREQUENCY) {
-    return true;
-  }
-
-
-  for (let key in entry.results) {
-    const val = entry.results[key]; 
-
-    let node = new TraverseNode(by_def, entry, val, traversion.level);
-    if (TRAVERSE_SIMILAR) {
-      traversion.wordsbreadthfirst.push.apply(traversion.wordsbreadthfirst, node.words);
-    } else {
-      traversion.wordsbreadthfirst.push.apply(traversion.wordsbreadthfirst, node.synonyms);
-    }
-  }
-
-  return true;
-}
-
-async function traverseCluster(tresult, word) {
-
-  let traversion = {
-    level : 1,
-    wordsbreadthfirst : [word]
-  };
-  tresult.noWords = 0;
-  tresult.master = await loadSingleWord(word, true);
-
-  do {
-    var previouslevelchildwords = traversion.wordsbreadthfirst.concat([]);
-    traversion.wordsbreadthfirst = [];
-
-    let promises = [];
-    for (let w of previouslevelchildwords) {
-      if (tresult.by_w[w]) {
-      } else if (tresult.noWords >= MAX_WORDS) {
-        console.log("Level "+traversion.level+" finished. Completed.");
-        return;
-      } else {
-        tresult.noWords++;
-        tresult.by_w[w] = 1;
-        console.log(tresult.noWords + "/" + MAX_WORDS);
-    
-        let nodepromise = loadDictionaryAndChildren(tresult, w, traversion);
-        promises.push(nodepromise);
-      }
-    }
-    await Promise.all(promises);
-    console.log("Level "+traversion.level+" finished.");
-
-    traversion.level++;
-  } while (traversion.wordsbreadthfirst.length);
-
-}
-
-async function loadCluster(word, asobject) {
-
-  const cfpath = `cache/clusters/${word}`;
-  if (CACHE_CLUSTERS && fs.existsSync(cfpath)) {
-    let ijson = fs.readFileSync(cfpath).toString();
-    let result = JSON.parse(ijson);
-    console.log("From cache file/cluster "+cfpath+"  asobject:"+asobject+"...\n");
-
-    if (asobject) {
-      return result;
-    } else {
-      return ijson;
-    }
-
-  } else {
-
-    const by_def = {};
-    const by_w = {};
-    const by_key = [];
-    let tresult = {
-      by_def,
-      by_w    };
-    const entry = await traverseCluster(tresult, word);
-    by_key.push.apply(by_key, Object.values(by_def));
-    const cmp = (firstEl, secondEl) => {
-      return firstEl.key.localeCompare(secondEl.key);
-    };
-    by_key.sort(cmp);
-    for (let defobj of by_key) {
-      defobj.compress();
-    }
-    let result = {
-      frequency:tresult.master.frequency,
-      pronunciation:tresult.master.pronunciation,
-      noClusterEntries:by_key.length,
-      results:by_key
-    };
-
-    let cjson;
-    if (CACHE_CLUSTERS) {
-      cjson = JSON.stringify(result);
-      fs.writeFile(cfpath, cjson, (err) => {
-        if (err) {
-          console.error("Cluster file/cluster "+cfpath+"  write failure : "+err+"\n");
-        } else {
-          console.log("Cluster file/cluster "+cfpath+"  written successfully\n");
-        }
-      });
-    }
-
-    if (asobject) {
-      return result;
-    } else {
-      if (!cjson) {
-        cjson = JSON.stringify(result);
-      }
-      return cjson;
-    }
-  }
-}
 
 
 export async function handler(event, context) {
+  crawler.initCrawler(
+    MAX_WORDS,
+    CACHE_CLUSTERS,
+    MAX_NODE_FREQUENCY,
+    TRAVERSE_SIMILAR
+    );
 
   //   extract the word query parameter from the HTTP request
   const word = event.queryStringParameters.word || "";
@@ -287,21 +23,14 @@ export async function handler(event, context) {
 
   try {
 
-    if (!fs.existsSync("cache/words")){
-      fs.mkdirSync("cache/words", { recursive: true });
-    }
-    if (!fs.existsSync("cache/clusters")){
-      fs.mkdirSync("cache/clusters");
-    }
-
     let json;
     if (synonym_cluster) {
       console.log("synonym_cluster:"+word);
 
-      json = await loadCluster(word, false);
+      json = await crawler.loadCluster(word, false);
     } else {
 
-      json = await loadSingleWord(word, false);
+      json = await crawler.loadSingleWord(word, false);
     }
 
 
