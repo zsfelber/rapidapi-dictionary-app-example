4459001bd833ecf0f76b618ff4ec93295b616cae
zsfelber <zsfelber@gmail.com>
2022-02-09 09:50:32 +0100

    ..

--- functions/crawl-background.js
+++ functions/crawl-background.js
@@ -23,6 +23,27 @@ export async function handler(event, context) {
     const cw4 = require('./include/common-words-10000-p-r.js');
     const cw5 = require('./include/common-words-10000-s-z.js');
     const cws = [cw1, cw2, cw3, cw4, cw5]; 
+    const cs = [];
+    a:for (let cw of cws) {
+      for (let commonWord in cw.TheMostCommon10000) {
+        cs.push(commonWord);
+      }
+    }
+
+    // sort randomly
+    let rnd = new Date().getMilliseconds()+Math.random()*100;
+    String.prototype.hashCode = function(){
+      var hash = 0;
+      for (var i = 0; i < this.length; i++) {
+          var character = (this.charCodeAt(i)+rnd)&0xff;
+          hash = ((hash<<5)-hash)+character;
+          hash = hash & hash; // Convert to 32bit integer
+      }
+      return hash;
+    }
+    cs.sort((a,b)=>{
+      return a.hashCode()-b.hashCode();
+    });
 
     const by_def = {};
     const by_w = {};
@@ -33,21 +54,20 @@ export async function handler(event, context) {
     tresult.newWords = 0;
 
     let promises = [];
-    a:for (let cw of cws) {
-      for (let commonWord in cw.TheMostCommon10000) {
-        let trpromise = crawler.traverseCluster(tresult, commonWord, false, true);
-        promises.push(trpromise);
+    for (let commonWord of cs) {
+      let trpromise = crawler.traverseCluster(tresult, commonWord, false, true);
+      promises.push(trpromise);
 
-        if (promises.length >= 10) {
-          await Promise.all(promises);
-          promises = [];
-          if (crawler.isApiLimitReached()) {
-            console.log("API limit reached. STOP whole crawling");
-            break a;
-          }
+      if (promises.length >= 10) {
+        await Promise.all(promises);
+        promises = [];
+        if (crawler.isApiLimitReached()) {
+          console.log("API limit reached. STOP whole crawling");
+
         }
       }
     }
+
     await Promise.all(promises);
     promises = [];
 
