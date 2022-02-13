df1b0eb210d8550eb64c86773593981cf9825f8e
zsfelber <zsfelber@gmail.com>
2022-02-13 13:45:26 +0100

    ..

--- public/js/main.js
+++ public/js/main.js
@@ -1036,7 +1036,9 @@ $(document).ready(function(){
 
     if (!isElectron()) {
         console.log("It is not electron, hiding restricted pages.");
-        $(".restricted-electron").css('display', 'none');
+    } else {
+        console.log("It is electron, showing restricted pages.");
+        $(".restricted-electron").css('display', 'visible');
     }
     
     async function dosubmit(word) {
