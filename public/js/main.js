a0a75d70bb521e990a3aa03272d7e0c703d9609b
zsfelber <zsfelber@gmail.com>
2022-02-13 11:35:59 +0100

    ..

--- public/js/main.js
+++ public/js/main.js
@@ -94,27 +94,6 @@ var checkboxdata = {
     },
 };
 
-function isElectron() {
-    // https://stackoverflow.com/questions/61725325/detect-an-electron-instance-via-javascript
-
-    // Renderer process
-    if (typeof window !== 'undefined' && typeof window.process === 'object' && window.process.type === 'renderer') {
-        return true;
-    }
-
-    // Main process
-    if (typeof process !== 'undefined' && typeof process.versions === 'object' && !!process.versions.electron) {
-        return true;
-    }
-
-    // Detect the user agent when the `nodeIntegration` option is set to true
-    if (typeof navigator === 'object' && typeof navigator.userAgent === 'string' && navigator.userAgent.indexOf('Electron') >= 0) {
-        return true;
-    }
-
-    return false;
-}
-
 async function fetchWord(word, mode, qs=[]) {
     let apis=[];
     if (isch("WORDSAPI")) apis.push("wordsapi");
